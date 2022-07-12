8b00a9a936b9b962e3e0489534f47d6a72746953
Rubenia Borge <rubeniaolaska@gmail.com>
2022-07-12 18:59:41 -0500

    Update Item.cpp

--- Item.cpp
+++ Item.cpp
@@ -5,8 +5,11 @@
 #include "Item.h"
 #include <string>
 
-Item::Item() {itemCode = "", itemPrice = 1.0};
+Item::Item() {itemCode = "ND", itemPrice = 1.0};
 Item::Item(string:: code, float price) {itemCode = code; itemPrice = price};
 
 void Item::SetItemCode (std::string code) {this-> itemCode = code};
 void Item::SetItemPrice (int price) {this-> itemPrice = price};
+
+std::string Item::GetItemCode() {return itemCode};
+float Item::GetItemPrice() {return itemPrice};
