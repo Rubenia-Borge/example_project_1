6019dd34d6baaedc420cb3ab75c2a855c0bd6060
Rubenia Borge <rubeniaolaska@gmail.com>
2022-07-12 18:20:42 -0500

    Update Item.h

--- Item.h
+++ Item.h
@@ -1,1 +1,24 @@
 
+#ifndef ITEM_H
+#define ITEM_H
+
+#include <string>
+
+class Item{
+    public:
+      Item();
+      Item(std::string itemCode, float itemPrice);
+      
+      void SetItemCode (std::string itemCode);
+      void SetItemPrice (float itemPrice);
+      
+      std::string GetItemCode();
+      float GetItemPrice();
+      
+    private:
+      std::string code;
+      int price;
+     
+}
+
+#endif
