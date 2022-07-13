d62bfe23a0fc0a9fb1ff204411f7ab5794eb6659
Rubenia Borge <rubeniaolaska@gmail.com>
2022-07-12 19:10:18 -0500

    Update Items.h

--- Items.h
+++ Items.h
@@ -1,1 +1,19 @@
 
+#ifndef ITEMS_H
+#define ITEMS_H
+
+#include <vector>
+#include "Item.h"
+
+class Items{
+    public:
+      Items();
+      
+      void AddItem();
+      void EditItem();
+      void DeleteItem();
+      void ItemSearch();
+      
+};
+
+#endif
