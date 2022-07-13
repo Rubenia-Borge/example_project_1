d715caa611a61c2776c4b8a495005cecfa6748f5
Rubenia Borge <rubeniaolaska@gmail.com>
2022-07-12 23:49:30 -0500

    Update Items.cpp

--- Items.cpp
+++ Items.cpp
@@ -1,1 +1,13 @@
 
+#include <vector>
+#include <iomanip>
+#include <fstream>
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include "Item.h"
+#include "Items.h"
+
+extern Items items;
+
+Items::Items() {std::vector<Item> itemList;};
